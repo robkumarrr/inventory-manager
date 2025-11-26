d34271d77666ac9e8a1933d6507feab0130c2298
robertkumar <rob_kumar@outlook.com>
2025-11-26 15:49:00 -0500

    Added HasFactory trait to teh InventoryItem model

--- app/Models/InventoryItem.php
+++ app/Models/InventoryItem.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class InventoryItem extends Model
 {
+    Use HasFactory;
     /**
      * Properties of the Model that are fillable.
      *

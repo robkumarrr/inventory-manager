c622c56f67917f790fc921beb2697edcf3ab8136
robertkumar <rob_kumar@outlook.com>
2025-12-09 13:15:29 -0500

    Refactoring the dashboard to show the form instead of the welcome page

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/AppLayout.vue';
 import { dashboard } from '@/routes';
-import { type BreadcrumbItem } from '@/types';
-import { Head } from '@inertiajs/vue3';
+import { type BreadcrumbItem, InventoryItem, InventoryItemForm } from '@/types';
+import { Head, Link, useForm } from '@inertiajs/vue3';
 import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+import { route } from 'ziggy-js';
+//TODO: refactor to make dashboard the main view of the table, which should've been done in the first place
+
+const formData = useForm<InventoryItemForm>({
+    name: '',
+    quantity: 0,
+    sku: '',
+    notification_sent: false,
+});
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
